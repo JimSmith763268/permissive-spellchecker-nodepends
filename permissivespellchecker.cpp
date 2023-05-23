54f9e285345e7866d45daa388b88722bb6b5b8bd
JimSmith763268 <zendagiwaqi@gmail.com>
2023-05-23 17:53:55 +0300

    First commit

---
+++ permissivespellchecker.cpp
@@ -0,0 +1,11 @@
+#include "permissivespellchecker.h"
+
+bool approve(const char *string)
+{
+    return true;
+}
+
+bool reject(const char *string)
+{
+    return false;
+}
---
+++ permissivespellchecker.h
@@ -0,0 +1,7 @@
+#ifndef PERMISSIVESPELLCHECKER_H
+#define PERMISSIVESPELLCHECKER_H
+
+__declspec(dllexport) bool approve(const char * string);
+__declspec(dllexport) bool reject(const char * string);
+
+#endif // PERMISSIVESPELLCHECKER_H
